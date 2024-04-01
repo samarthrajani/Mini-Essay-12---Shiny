7329610d5e301ec4e21cde71ee2b7483eb6a540b
Samarth Rajani <samarth.rajani@mail.utoronto.ca>
2024-04-01 18:35:49 -0400

    Localised data

--- importing data.R
+++ importing data.R
@@ -1,4 +1,6 @@
-dataset <- data.frame(read.csv('/Users/Samarthsumitrajani/Downloads/Auschwitz_Death_Certificates_1942-1943 - Auschwitz.csv'))
-unique_nationalities <- table(dataset$Birthplace)
-sorted_nationality_counts <- sort(unique_nationalities, decreasing = TRUE)
-sorted_nationality_counts
+data <- data.frame(read.csv('/Users/Samarthsumitrajani/Downloads/Auschwitz_Death_Certificates_1942-1943 - Auschwitz.csv'))
+data
+Birthplace_counts <- table(data$Birthplace)
+Birthplace_counts_dataframe <- as.data.frame(Birthplace_counts)
+names(Birthplace_counts_dataframe) <- c("Birthplace", "Count")
+write_csv(Birthplace_counts_dataframe, "~/Mini-Essay-12---Shiny/mini_essay_data.csv")
